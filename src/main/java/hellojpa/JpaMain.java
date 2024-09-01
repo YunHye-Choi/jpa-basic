76554a57dde3295dd102fdc4a356573312e570fb
YunHye-Choi <anffl98@gmail.com>
2024-09-01 16:12:08 +0900

    [feat] JPA 시작하기 - em 생성하여 기본적인 crud 사용법 알아보기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,33 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
-        em.close();
+        try{
+            // 회원 추가
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("Anne");
+            em.persist(member);
+
+            // 회원 수정
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("Joy");
+
+            // 회원 삭제
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(member);
+
+            // 회원 조회
+//            Member findMember = em.find(Member.class, 1L);
+
+        } catch (Exception e){
+            tx.rollback();
+        } finally{
+            em.close();
+        }
+
         emf.close();
     }
 }
