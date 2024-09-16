e2ffaf2a4a97cf4f8be1caba7e96ab9c8d68dd13
YunHye-Choi <anffl98@gmail.com>
2024-09-16 23:30:08 +0900

    [feat] GeneratedValue - SEQUENCE 전략

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,21 +12,16 @@ public class JpaMain {
         tx.begin();
 
         try{
-            // 비영속 상태
             Member member = new Member();
-            member.setId(1L);
-            member.setName("Anne");
+            member.setUsername("userA");
 
-            // 영속 상태 - 이 시점에는 쿼리가 실행되지 않음!
+            System.out.println("==== start calling nextval of seq ====");
             em.persist(member);
+            System.out.println("==== end calling nextval of seq ====");
 
+            System.out.println("==== COMMIT! start executing insert query ==== ");
             tx.commit(); // 이 시점에 쿼리가 날아감!
-
-            // 준영속 - 회원 엔티티를 영속성 컨텍스트에서 분리
-            em.detach(member);
-
-            // 삭제 - 객체를 삭제한 상태
-            em.remove(member);
+            System.out.println("==== end executing insert query ==== ");
 
         } catch (Exception e){
             tx.rollback();
